42509080a2e6ee01e36f3c4046a2d3d59f28eacb
Sai Chaithanyia p <chaithanyiap@gmail.com>
2026-06-10 19:14:31 +0530

    minor change

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def explanation():
         yield "data: [DONE]\n\n"
     return Response(stream_with_context(generate()), mimetype='text/event-stream')
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=7860)
+    port=int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port)
